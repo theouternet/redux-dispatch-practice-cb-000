bcdf79e4bc63995c1c8a72ac2c2722049cb4bcea
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-28 21:11:52 +0000

    Automatically backed up by Learn

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -10,3 +10,24 @@ export function dispatch(){
 export function render(){
 
 }
+
+
+    let state;
+    function changeState(state = { count: 0 }, action) {
+        switch (action.type) {
+          case 'INCREASE_COUNT':
+            return { count: state.count + 1 }
+          default:
+            return state;
+        }
+      }
+      function dispatch(action){
+      state = changeState(state, action)
+      render()
+    }
+     
+    function render(){
+      document.body.textContent = state.count
+    }
+     
+    dispatch({type: '@@INIT'})
