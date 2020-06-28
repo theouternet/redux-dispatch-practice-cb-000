c8568a51313f64ab518572d6cbe02943e5fdfb3a
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-28 21:15:34 +0000

    Automatically backed up by Learn

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -6,6 +6,7 @@ export function managePets(state = { pets: []}, action){
     case 'ADD_PET':
       return { pets: [...state.pets, action.pet] )
       case 'REMOVE_PET':
+        return { pets: state.pets.filter(pet => pet.id !==)
   }
 }
 
